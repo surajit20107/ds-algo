ada8aaa989253f62bded8ba091e521764845ac79
Surajit <surajitjana20107@gmail.com>
2026-06-10 15:21:12 +0000

    added optimum version

--- segregate0and1.js
+++ segregate0and1.js
@@ -1,19 +1,30 @@
-// function segregate0and1(arr) {
-//     for (let i = 0; i < arr.length - 1; i++) {
-//         for (let j = arr.length - 1;j > i; j--) {
-//             if (arr[i] === 1) {
-//                 [arr[i], arr[j]] = [arr[j], arr[i]];
-//             }
-//         }
-//     }
-//     return arr;
-// }
+// Problem: https://www.geeksforgeeks.org/problems/segregate-0s-and-1s5106/1
 
+// Time: O(n^2), Space: O(1)
 function segregate0and1(arr) {
-    for (let i = 0; i < arr.length - 1; i++) {
-        if (arr[i])
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = arr.length - 1; j > i; j--) {
+            if (arr[i] === 1) {
+                [arr[i], arr[j]] = [arr[j], arr[i]];
+            }
+        }
     }
-    return arr;
 }
 
-console.log(segregate0and1([0, 1, 0, 1, 0, 0, 1, 1, 1, 0]));
+// Time: O(n), Space: O(1)
+function segregate0and1(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+    while (left < right) {
+        if (arr[left] === 0) {
+            left++;
+        } else {
+            [arr[left], arr[right]] = [arr[right], arr[left]];
+            right--;
+        }
+    }
+}
+
+const arr = [0, 1, 0, 1, 0, 0, 1, 1, 1, 0];
+segregate0and1(arr);
+console.log(arr);
