5616b933febe0f1b53a697c0d33efe98ef10f70f
Surajit <surajitjana20107@gmail.com>
2026-06-23 11:11:40 +0530

    Time: 14 ms (60.53%), Space: 57 MB (56.51%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function (nums, target) {
+    nums.sort((a, b) => a - b);
+    let closestSum = Infinity;
+    let minDiff = Infinity;
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            const currentSum = nums[i] + nums[left] + nums[right];
+            const currentDiff = Math.abs(currentSum - target);
+
+            if (currentDiff < minDiff) {
+                minDiff = currentDiff;
+                closestSum = currentSum;
+            }
+
+            if (currentSum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+
+    return closestSum;
+};
