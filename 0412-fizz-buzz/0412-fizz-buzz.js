047633665dfed37021d07c51a78ca1c655189560
Surajit <surajitjana20107@gmail.com>
2026-06-27 21:29:43 +0530

    Time: 1 ms (56.24%), Space: 57.8 MB (62.24%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.js
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -2,17 +2,13 @@
  * @param {number} n
  * @return {string[]}
  */
-var fizzBuzz = function(n) {
-  let answer = []
-  for (let i = 1; i <= n; i++) {
-    let res = ""
-    if (i % 3 === 0) {
-      res += "Fizz"
+var fizzBuzz = function (n) {
+    let answer = [];
+    for (let i = 1; i <= n; i++) {
+        let res = "";
+        if (i % 3 === 0) res += "Fizz";
+        if (i % 5 === 0) res += "Buzz";
+        answer.push(res || String(i));
     }
-    if (i % 5 === 0) {
-      res += "Buzz"
-    }
-    answer.push(res || String(i))
-  }
-  return answer
+    return answer;
 };
