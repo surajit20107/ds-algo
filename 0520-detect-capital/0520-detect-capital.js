25cb6dff7e6ed63236bab94bb09508c54ebd06ae
Surajit <surajitjana20107@gmail.com>
2026-07-01 15:25:10 +0530

    Time: 0 ms (100%), Space: 53.7 MB (86.73%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var detectCapitalUse = function(word) {
+    let count = 0;
+    const n = word.length;
+    for (let i=0;i < n; i++) {
+        if (word.charCodeAt(i) <= 90) {
+            count++;
+        }
+    }
+    return count === n || count === 0 || count === 1 && word.charCodeAt(0) <= 90;
+};
