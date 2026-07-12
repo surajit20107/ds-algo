9482796a045def39c94109c56db131820631983b
Surajit <surajitdev20107@gmail.com>
2026-07-12 19:35:19 +0530

    Refactor countTriplets method for clarity

--- GFG/TripletSumSmaller.js
+++ GFG/TripletSumSmaller.js
@@ -1,34 +1,27 @@
 /**
- * gfg - https://www.geeksforgeeks.org/problems/count-triplets-with-sum-smaller-than-x5549/1
- * Time Complexity: O(n²)
- * Space Complexity: O(1)
+ * @param {number} sum
+ * @param {number[]} arr
+ * @return {number}
  */
 
 class Solution {
     countTriplets(sum, arr) {
-        arr.sort((a, b) => a - b);
-        let res = 0;
-        for (let i = 0; i < arr.length - 2; i++) {
-            let j = i + 1;
-            let k = arr.length - 1;
-            while (j < k) {
-                let val = arr[i] + arr[j] + arr[k];
+        // code here
+        arr.sort((a, b) => a - b)
+        let res = 0
+        for (let i=0;i<arr.length-2;i++){
+            let j = i+1
+            let k=arr.length-1
+            while (j<k) {
+                let val = arr[i]+arr[j]+arr[k]
                 if (val >= sum) {
-                    k--;
+                    k--
                 } else {
-                    res += k - j;
-                    j++;
+                    res += k - j
+                    j++
                 }
             }
         }
-        return res;
+        return res
     }
 }
-const solution = new Solution();
-
-// test cases
-console.log(solution.countTriplets(12, [-2, 0, 1, 3]));  // 4
-console.log(solution.countTriplets(100, [5, 1, 3, 4, 7]));  // 10
-console.log(solution.countTriplets(10, [1, 2, 3, 4, 5]));  // 6
-console.log(solution.countTriplets(2, [-2, 0, 1, 3])); // 2
-console.log(solution.countTriplets(12, [5, 1, 3, 4, 7])); // 4
