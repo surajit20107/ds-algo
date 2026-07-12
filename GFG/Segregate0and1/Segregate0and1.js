da29a781d182a274e43dc9eb45fc435d92ee26c1
Surajit <surajitdev20107@gmail.com>
2026-07-12 19:32:06 +0530

    Refactor segregation logic into class method
    Refactored the segregation logic into a class method and removed the brute force implementation.

--- GFG/Segregate0and1/Segregate0and1.js
+++ GFG/Segregate0and1/Segregate0and1.js
@@ -1,30 +1,18 @@
-// Time: O(n^2), Space: O(1)
-// Brute Force
-function segreGate0and1(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = arr.length - 1; j > i; j--) {
-            if (arr[i] === 1) {
-                [arr[i], arr[j]] = [arr[j], arr[i]];
-            }
-        }
-    }
+/**
+* @param {number[]} arr
+*/
+class Solution {
+	segregate0and1(arr) {
+		// code here
+		let left = 0;
+		let right = arr.length - 1;
+		while (left < right) {
+		    if (arr[left] === 0)  {
+		        left++;
+		    } else {
+		        [arr[left], arr[right]] = [arr[right], arr[left]];
+		        right--;
+		    }
+		}
+	}
 }
-
-// Time: O(n), Space: O(1)
-// Optimum
-function segregate0and1(arr) {
-    let left = 0;
-    let right = arr.length - 1;
-    while (left < right) {
-        if (arr[left] === 0) {
-            left++;
-        } else {
-            [arr[left], arr[right]] = [arr[right], arr[left]];
-            right--;
-        }
-    }
-}
-
-const arr = [0, 1, 0, 1, 0, 0, 1, 1, 1, 0];
-segregate0and1(arr);
-console.log(arr);
