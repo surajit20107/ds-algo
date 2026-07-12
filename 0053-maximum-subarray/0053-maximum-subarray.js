a8b3f02427fc0bfc6edfa6b1039b0a0b3749b8d7
Surajit <surajitjana20107@gmail.com>
2026-07-12 18:23:49 +0530

    Time: 1 ms (87.21%), Space: 63.7 MB (66.6%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -2,13 +2,13 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function (nums) {
+var maxSubArray = function(nums) {
     let sum = 0;
-    let max = -Infinity;
+    let res = -Infinity;
     for (let i = 0; i < nums.length; i++) {
-        sum += nums[i]
-        max = Math.max(sum, max)
-        sum = Math.max(sum, 0)
+        sum = sum + nums[i];
+        res = Math.max(res, sum);
+        sum = Math.max(sum, 0);
     }
-    return max
+    return res
 };
