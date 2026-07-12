87771ca01d4de7e04405592f858bcdb1190f6381
Surajit <surajitjana20107@gmail.com>
2026-07-12 21:34:48 +0530

    Time: 4 ms (49.02%), Space: 54.6 MB (68.21%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.js
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.js
@@ -2,17 +2,17 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxProduct = function(nums) {
+var maxProduct = function (nums) {
     let ans = nums[0];
     let minEnding = nums[0];
     let maxEnding = nums[0];
-     for (let i = 1; i < nums.length; i++) {
+    for (let i = 1; i < nums.length; i++) {
         let curr = nums[i];
         let maxPrev = maxEnding;
         let minPrev = minEnding;
-        maxEnding = Math.max(curr, curr*maxPrev, curr*minPrev);
-        minEnding = Math.min(curr, curr*maxPrev, curr*minPrev);
+        maxEnding = Math.max(curr, curr * maxPrev, curr * minPrev);
+        minEnding = Math.min(curr, curr * maxPrev, curr * minPrev);
         ans = Math.max(ans, maxEnding);
-     }
-     return ans;
+    }
+    return ans;
 };
