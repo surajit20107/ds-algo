3222b44af327c08aa28ba20e36982a1b8764727c
Surajit <surajitjana20107@gmail.com>
2026-07-18 13:19:06 +0530

    Time: 2 ms (61.03%), Space: 55.4 MB (89.34%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.js
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -7,12 +7,14 @@ var canPlaceFlowers = function (flowerbed, n) {
     const length = flowerbed.length;
 
     for (let i = 0; i < length; i++) {
-        if (flowerbed[i] === 0 &&
-            (i === 0 || flowerbed[i - 1] === 0) &&
-            (i === length - 1 || flowerbed[i + 1] === 0)) {
-                flowerbed[i] = 1;
-                n--;
-                i++;
+        if (
+            flowerbed[i] === 0 && // current index is zero ?
+            (i === 0 || flowerbed[i - 1] === 0) && // left neighbour is 0 ?
+            (i === length - 1 || flowerbed[i + 1] === 0) // right neighbour is 0 ?
+        ) {
+            flowerbed[i] = 1;
+            n--;
+            i++; // skip the next neighbour because here planted
         }
     }
     return n <= 0;
