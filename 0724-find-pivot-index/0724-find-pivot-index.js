e87f606685b2acaaba4b7197db408594e705427c
Surajit <surajitjana20107@gmail.com>
2026-07-20 20:23:44 +0530

    Time: 3 ms (48.4%), Space: 59.3 MB (15.68%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+    let prefix = new Array(nums.length);
+    let suffix = new Array(nums.length);
+
+    prefix[0] = 0;
+    for (let i = 1; i < nums.length; i++) {
+        prefix[i] = prefix[i - 1] + nums[i - 1];
+    }
+
+    suffix[nums.length - 1] = 0;
+    for (let i = nums.length - 2; i >= 0; i--) {
+        suffix[i] = suffix[i + 1] + nums[i + 1];
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        if (prefix[i] === suffix[i]) return i;
+    }
+    return -1;
+};
