4fad1ae2f257cbe947bb144dcd6542f0ab325106
Surajit <surajitjana20107@gmail.com>
2026-07-23 21:36:20 +0530

    Time: 4 ms (98.43%), Space: 54.9 MB (88.16%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+
+    let arr = new Array(26).fill(0);
+    for (let i = 0; i < s.length; i++) {
+        arr[s.charCodeAt(i) - 97]++;
+        arr[t.charCodeAt(i) - 97]--;
+    }
+    return arr.every(val => val === 0);
+};
