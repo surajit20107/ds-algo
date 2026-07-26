5cebacf87106ef41ae99f29ede844389d58b9f4f
Surajit <surajitjana20107@gmail.com>
2026-07-26 11:21:17 +0530

    Time: 5 ms (51.54%), Space: 24 MB (20.3%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -6,7 +6,7 @@ public:
         vector<vector<int>> ans;
         int start1 = intervals[0][0];
         int end1 = intervals[0][1];
-        
+
         for (int i = 1; i < intervals.size(); i++) {
             int start2 = intervals[i][0];
             int end2 = intervals[i][1];
@@ -20,7 +20,7 @@ public:
                 end1 = max(end1, end2);
             }
         }
-
+        
         ans.push_back({start1, end1});
         return ans;
     }
