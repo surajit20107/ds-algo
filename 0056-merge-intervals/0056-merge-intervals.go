19af3ac54707a2f9ff93aa3eae9029fdb456ed13
Surajit <surajitjana20107@gmail.com>
2026-07-27 09:30:53 +0530

    Time: 11 ms (7.21%), Space: 8 MB (96.2%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.go
@@ -0,0 +1,24 @@
+func merge(intervals [][]int) [][]int {
+    sort.Slice(intervals, func(i, j int) bool {
+        return intervals[i][0] < intervals[j][0]
+    })
+    ans := [][]int{}
+    start1 := intervals[0][0]
+    end1 := intervals[0][1]
+
+    for i := 1; i < len(intervals); i++ {
+        start2 := intervals[i][0]
+        end2 := intervals[i][1]
+
+        if end1 < start2 {
+            ans = append(ans, []int{start1, end1})
+            start1 = start2
+            end1 = end2
+        } else {
+            start1 = min(start1, start2)
+            end1 = max(end1, end2)
+        }
+    }
+    ans = append(ans, []int{start1, end1})
+    return ans
+}
