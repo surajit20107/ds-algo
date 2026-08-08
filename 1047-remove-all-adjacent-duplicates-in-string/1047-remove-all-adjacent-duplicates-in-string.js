71f0156f05ca211efd743d0fdcd1d7da33a24513
Surajit <surajitjana20107@gmail.com>
2026-08-08 12:15:45 +0530

    Time: 17 ms (16.69%), Space: 63.1 MB (41.28%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeDuplicates = function(s) {
+  let stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    const top = stack[stack.length - 1];
+    if (top === s[i]) stack.pop();
+    else stack.push(s[i]);
+  }
+
+  return stack.join("");
+};
