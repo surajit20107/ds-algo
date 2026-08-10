36984566eefaf569e338c5d37a3685c062daa20c
Surajit <surajitjana20107@gmail.com>
2026-08-10 21:28:26 +0530

    Time: 16 ms (68.36%), Space: 81.1 MB (17.03%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function(temperatures) {
+    const stack = [];
+    const res = new Array(temperatures.length).fill(0);
+
+    for (let i = temperatures.length - 1; i >= 0; i--) {
+        while (stack.length > 0 && temperatures[stack[stack.length-1]] <= temperatures[i]) {
+            stack.pop();
+        }
+
+        if (stack.length === 0) {
+            res[i] = 0;
+        } else {
+            res[i] = stack[stack.length - 1] - i
+        }
+        stack.push(i)
+    }
+
+    return res;
+};
