2c6760138506a54135301178e51ce826f424ab98
Surajit <surajitjana20107@gmail.com>
2026-08-12 19:33:50 +0530

    Time: 3 ms (74.58%), Space: 29.8 MB (7.52%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> res(n, -1);
-        stack<int> st;
-
-        for (int i = 2 * n - 1; i >= 0; i--) {
-            int index = i % n;
-
-            while (!st.empty() && nums[index] >= st.top()) {
-                st.pop();
-            }
-
-            if (i < n) {
-                res[index] = st.empty() ? -1 : st.top();
-            }
-
-            st.push(nums[index]);
+        vector<int> res;
+        vector<int> st;
+        // Pushing the elements in stack to simulate the circular array
+        // So after last element my array can see the first element for once using stack
+        for (int i = nums.size() - 2; i >= 0; i--) {
+            st.push_back(nums[i]);
         }
-        
+        // finding greater elements using nums and stack
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            while (st.size() > 0 && st.back() <= nums[i]) {
+                st.pop_back();
+            }
+
+            if (st.size() == 0) {
+                res.push_back(-1);
+            } else {
+                res.push_back(st.back());
+            }
+            st.push_back(nums[i]);
+        }
+        reverse(res.begin(), res.end());
         return res;
     }
 };
