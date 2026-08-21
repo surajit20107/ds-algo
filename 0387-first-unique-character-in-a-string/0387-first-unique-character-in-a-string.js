5f209ddbbd159a27cf2be98443b384518cdf02f6
Surajit <surajitjana20107@gmail.com>
2026-08-21 09:12:02 +0530

    Time: 8 ms (98.2%), Space: 60.3 MB (37.42%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -3,13 +3,17 @@
  * @return {number}
  */
 var firstUniqChar = function(s) {
-    const map = new Map()
-    for (let char of s) {
-        map.set(char, (map.get(char) || 0) + 1);
-    }
-
+    const freq = new Array(26).fill(0);
+    const aCode = 'a'.charCodeAt(0);
+    
     for (let i = 0; i < s.length; i++) {
-        if (map.get(s[i]) === 1) return i;
+        freq[s.charCodeAt(i) - aCode]++;
+    }
+    
+    for (let i = 0; i < s.length; i++) {
+        if (freq[s.charCodeAt(i) - aCode] === 1) {
+            return i;
+        }
     }
     
     return -1;
