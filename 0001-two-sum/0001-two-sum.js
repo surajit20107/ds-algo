3435b42766e3c416bd62f97760480a0f40c13d09
Surajit <surajitjana20107@gmail.com>
2026-08-27 12:39:01 +0530

    Time: 1 ms (88.85%), Space: 54.1 MB (81.91%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,14 +4,17 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-    if (nums.length === 2) return [0, 1];
     const map = new Map();
+
     for (let i = 0; i < nums.length; i++) {
-        const complement = target - nums[i];
-        if (map.has(complement)) {
-            return [map.get(complement), i];
+        const need = target - nums[i];
+
+        if (map.has(need)) {
+            return [map.get(need), i];
         }
+
         map.set(nums[i], i);
     }
+    
     return [];
 };
