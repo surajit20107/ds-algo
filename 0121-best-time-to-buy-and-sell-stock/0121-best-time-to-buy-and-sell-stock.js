fb497ee2b02ed825392fedb7f2e859dba9731d59
Surajit <surajitjana20107@gmail.com>
2026-08-27 12:10:49 +0530

    Time: 8 ms (8.89%), Space: 67.4 MB (5.35%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -5,9 +5,11 @@
 var maxProfit = function(prices) {
     let buy = prices[0];
     let profit = 0;
+
     for (let pr of prices) {
         buy = Math.min(buy, pr);
         profit = Math.max(profit, pr -  buy);
     }
+    
     return profit;
 };
